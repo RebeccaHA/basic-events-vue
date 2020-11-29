28dc682fa3a9bdfccacadf0cf994ad5e37bd5797
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-29 16:15:28 +0000

    added event listener to input for when changes @input then used event param to grab the value

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0
+      counter: 0,
+      name: ""
     };
   },
   methods: {
@@ -10,6 +11,9 @@ const app = Vue.createApp({
     },
     decreaseCounter() {
       this.counter--;
+    },
+    setName(e) {
+      this.name = e.target.value;
     }
   }
 });
